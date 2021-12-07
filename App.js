86f6d5938e4622210ce06ba69030429670f5124f
James Coker <jamescoker@Jamess-MacBook-Pro.local>
2021-12-07 16:58:10 +0000

    makes a start on app and navigation

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Header } from "./components/Header";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { HomeScreen } from "./components/HomeScreen";
+
+// export function HomeScreen() {
+//     return (
+//         <View
+//             style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+//         >
+//             <Text>Home Screen</Text>
+//         </View>
+//     );
+// }
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <Header style={{ justifyContent: "center" }} />
+            <Stack.Navigator>
+                {/* <View style={styles.container}> */}
+
+                <Stack.Screen
+                    name="Home"
+                    component={HomeScreen}
+                    options={{ title: "Welcome" }}
+                />
+                {/* <Stack.Screen name="Profile" component={ProfileScreen} /> */}
+                {/* </View> */}
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
---
+++ components/HomeScreen.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from "react-native";
+
+export const HomeScreen = ({ navigation }) => {
+    return (
+        <Button
+            title="Go to Jane's profile"
+            onPress={() => navigation.navigate("Profile", { name: "Jane" })}
+        />
+    );
+};
+const ProfileScreen = ({ navigation, route }) => {
+    return <Text>This is {route.params.name}'s profile</Text>;
+};
