f37e7214fcab03473a723f0171e173274c0c656f
JayAk12345 <jahanzaib50@hotmail.co.uk>
2021-12-08 14:35:44 +0000

    adds user context around return of App

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useContext } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Header } from "./components/Header";
 import { NavigationContainer } from "@react-navigation/native";
@@ -10,30 +10,34 @@ import { HomeScreen } from "./components/HomeScreen";
 import { WeekScreen } from "./components/WeekScreen";
 import { InfoScreen } from "./components/InfoScreen";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { userContext } from "./contexts/userContext";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+  const user = null;
+
   return (
-    <NavigationContainer>
-      <Header style={{ justifyContent: "center" }} />
+    <userContext.Provider value={user}>
+      <NavigationContainer>
+        <Header style={{ justifyContent: "center" }} />
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Your week" component={WeekScreen} />
+          <Tab.Screen name="Badges" component={BadgesScreen} />
+          <Tab.Screen name="Info" component={InfoScreen} />
+        </Tab.Navigator>
 
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Your week" component={WeekScreen} />
-        <Tab.Screen name="Badges" component={BadgesScreen} />
-        <Tab.Screen name="Info" component={InfoScreen} />
-      </Tab.Navigator>
-
-      {/* <Stack.Navigator>
+        {/* <Stack.Navigator>
         { <View style={styles.container}> */}
-      {/* <Stack.Screen name="Home" component={NavBar} /> */}
-      {/* <Stack.Screen name="Account" component={ProfileScreen} /> */}
-      {/* <Stack.Screen name="Badges" component={BadgesScreen} /> */}
-      {/* </View> */}
-      {/* </Stack.Navigator> */}
-    </NavigationContainer>
+        {/* <Stack.Screen name="Home" component={NavBar} /> */}
+        {/* <Stack.Screen name="Account" component={ProfileScreen} /> */}
+        {/* <Stack.Screen name="Badges" component={BadgesScreen} /> */}
+        {/* </View> */}
+        {/* </Stack.Navigator> */}
+      </NavigationContainer>
+    </userContext.Provider>
   );
 }
 
