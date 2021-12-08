d72998be23fe6b4d696b0d642e1f60a07ad49990
J-Coke <jamescoker@Jamess-MacBook-Pro.local>
2021-12-08 09:38:58 +0000

    testing my username has been changed

--- components/HomeScreen.jsx
+++ components/HomeScreen.jsx
@@ -5,7 +5,7 @@ export const HomeScreen = ({ navigation }) => {
     return (
         <Button
             title="Go to Jane's profile"
-            onPress={() => navigation.navigate("Profile", { name: "Jane" })}
+            onPress={() => navigation.navigate("Profile", { name: "James" })}
         />
     );
 };
