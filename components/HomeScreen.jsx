d80c361996ad89dc5807e6fe45b870d8ca701bf7
JayAk12345 <jahanzaib50@hotmail.co.uk>
2021-12-08 14:36:30 +0000

    brings in user context

--- components/HomeScreen.jsx
+++ components/HomeScreen.jsx
@@ -1,6 +1,10 @@
-import React from "react";
-import { Button, Text } from "react-native";
+import React, { useContext } from "react";
+import { Text } from "react-native";
+import { userContext } from "../contexts/userContext";
 
-export const HomeScreen = ({ navigation }) => {
+export const HomeScreen = ({}) => {
+  const user = useContext(userContext);
+  console.log(user);
+
   return <Text>Homepage</Text>;
 };
