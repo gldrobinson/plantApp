f5a4dabe3285a453171c807dc7907bd8d3beb310
J-Coke <1jamescoker1@gmail.com>
2021-12-09 10:44:54 +0000

    adds login funtionality and toggles overlay visibility

--- components/SignInOverlay.jsx
+++ components/SignInOverlay.jsx
@@ -3,40 +3,45 @@ import { View, Text, TextInput, Button } from "react-native";
 import { Overlay } from "react-native-elements";
 import { useForm } from "react-hook-form";
 import { userContext } from "../contexts/userContext";
+// import { updateUser } from "../contexts/userContext";
 
 export const SignInOverlay = () => {
-  const [visible, setVisible] = useState(true);
-  const [user, setUser] = useContext(userContext);
-  const [userLogin, setUserLogin] = useState("");
+    const [visible, setVisible] = useState(true);
 
-  const toggleOverlay = () => {
-    setVisible(!visible);
-  };
+    const [userLogin, setUserLogin] = useState("");
+    const { user, updateUser } = useContext(userContext);
+    const toggleOverlay = () => {
+        if (user === null) {
+            setVisible(true);
+        } else {
+            setVisible(false);
+        }
+    };
 
-  //   useEffect(() => {
-  //     toggleOverlay;
-  //   }, [user]);
+    useEffect(() => {
+        console.log(user);
+        toggleOverlay();
+    }, [user]);
 
-  const handlePress = () => {
-    setUser(userLogin);
-    console.log(user);
-  };
+    const handlePress = () => {
+        updateUser(userLogin);
+    };
 
-  return (
-    <View>
-      <Overlay isVisible={visible}>
-        <Text>Please input your username:</Text>
-        <TextInput
-          onChange={(e) => {
-            const input = e.nativeEvent.text;
-            setUserLogin(input);
-          }}
-          style={{ height: 30, borderColor: "gray", borderWidth: 1 }}
-        ></TextInput>
-        <Button onPress={handlePress}>Submit</Button>
-      </Overlay>
-    </View>
-  );
+    return (
+        <View>
+            <Overlay isVisible={visible}>
+                <Text>Please input your username:</Text>
+                <TextInput
+                    onChange={(e) => {
+                        const input = e.nativeEvent.text;
+                        setUserLogin(input);
+                    }}
+                    style={{ height: 30, borderColor: "gray", borderWidth: 1 }}
+                ></TextInput>
+                <Button onPress={handlePress}>Submit</Button>
+            </Overlay>
+        </View>
+    );
 };
 
 //onBackdropPress={toggleOverlay}
