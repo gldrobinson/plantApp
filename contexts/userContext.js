1bf7962eb17fe04062c7159b0bb0285aedd6f683
J-Coke <1jamescoker1@gmail.com>
2021-12-09 10:45:48 +0000

    gets usercontext working

--- contexts/userContext.js
+++ contexts/userContext.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { useContext } from "react";
 
 export const userContext = React.createContext();
-
+// export const updateUser = useContext(userContext);
 export const userProvider = (props) => {
-  //const [user, setUser] = useState("Jay");
-  return (
-    <userContext.Provider value={user}>{props.children}</userContext.Provider>
-  );
+    //const [user, setUser] = useState("Jay");
+    return (
+        <userContext.Provider value={user}>
+            {props.children}
+        </userContext.Provider>
+    );
 };
