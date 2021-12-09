5d4f65cf2569dad4b5a69c6018e4c10d49fead5e
J-Coke <1jamescoker1@gmail.com>
2021-12-09 11:57:24 +0000

    starts scrollview on your week

--- components/WeekScreen.jsx
+++ components/WeekScreen.jsx
@@ -1,6 +1,36 @@
 import React from "react";
-import { Button, Text } from "react-native";
+import { Button, ScrollView, Text, View, Image } from "react-native";
+// import banana from "../test-images";
+// import apple from "../test-images";
+// import orange from "../test-images";
+
+// import{apple}from
+// import{orange}from
 
 export const WeekScreen = ({ navigation }) => {
-  return <Text>Your week</Text>;
+    return (
+        <View>
+            <Text>Your week so far</Text>;
+            <ScrollView>
+                <Text>1</Text>
+                <Text>2</Text>
+                <Text>3</Text>
+                <Text>1</Text>
+                <Text>2</Text>
+                <Text>3</Text>
+                <Text>1</Text>
+                <Text>2</Text>
+                <Text>3</Text>
+                <Text>1</Text>
+                <Text>2</Text>
+                <Text>3</Text>
+                <Text>1</Text>
+                <Text>2</Text>
+                <Text>3</Text>
+                <Text>1</Text>
+                <Text>2</Text>
+                <Text>3</Text>
+            </ScrollView>
+        </View>
+    );
 };
