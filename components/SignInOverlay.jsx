186f2372920878469bde667b517ae983ff0e7c71
JayAk12345 <jahanzaib50@hotmail.co.uk>
2021-12-13 16:56:24 +0000

    adds setWeekcount and setCurrentStreak to handlePress, updates the state when user enters details

--- components/SignInOverlay.jsx
+++ components/SignInOverlay.jsx
@@ -3,45 +3,54 @@ import { View, Text, TextInput, Button } from "react-native";
 import { Overlay } from "react-native-elements";
 import { useForm } from "react-hook-form";
 import { userContext } from "../contexts/userContext";
+import { getUser } from "../Api/getApi";
 // import { updateUser } from "../contexts/userContext";
 
-export const SignInOverlay = () => {
-    const [visible, setVisible] = useState(true);
+export const SignInOverlay = ({
+  weekCount,
+  setWeekCount,
+  currentStreak,
+  setCurrentStreak,
+}) => {
+  const [visible, setVisible] = useState(true);
+  const [userLogin, setUserLogin] = useState("");
+  const { user, updateUser } = useContext(userContext);
 
-    const [userLogin, setUserLogin] = useState("");
-    const { user, updateUser } = useContext(userContext);
-    const toggleOverlay = () => {
-        if (user === null) {
-            setVisible(true);
-        } else {
-            setVisible(false);
-        }
-    };
+  const toggleOverlay = () => {
+    if (user === null) {
+      setVisible(true);
+    } else {
+      setVisible(false);
+    }
+  };
 
-    useEffect(() => {
-        console.log(user);
-        toggleOverlay();
-    }, [user]);
+  useEffect(() => {
+    toggleOverlay();
+  }, [user]);
 
-    const handlePress = () => {
-        updateUser(userLogin);
-    };
-
-    return (
-        <View>
-            <Overlay isVisible={visible}>
-                <Text>Please input your username:</Text>
-                <TextInput
-                    onChange={(e) => {
-                        const input = e.nativeEvent.text;
-                        setUserLogin(input);
-                    }}
-                    style={{ height: 30, borderColor: "gray", borderWidth: 1 }}
-                ></TextInput>
-                <Button title="Submit" onPress={handlePress}></Button>
-            </Overlay>
-        </View>
-    );
+  const handlePress = () => {
+    getUser(userLogin).then((user) => {
+      updateUser(user.username);
+      setWeekCount(user.currentWeek.length);
+      setCurrentStreak(user.streak.currentStreak);
+    });
+  };
+  console.log(weekCount);
+  return (
+    <View>
+      <Overlay isVisible={visible}>
+        <Text>Please input your username:</Text>
+        <TextInput
+          onChange={(e) => {
+            const input = e.nativeEvent.text;
+            setUserLogin(input);
+          }}
+          style={{ height: 30, borderColor: "gray", borderWidth: 1 }}
+        ></TextInput>
+        <Button title="Submit" onPress={handlePress}></Button>
+      </Overlay>
+    </View>
+  );
 };
 
 //onBackdropPress={toggleOverlay}
