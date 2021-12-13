a7ed7b7f713d205a8eafe642b5dfe9ae9cbe8dcd
Holly <hollykgreaves@gmail.com>
2021-12-13 15:42:46 +0000

    adds get badges function

--- components/BadgesScreen.jsx
+++ components/BadgesScreen.jsx
@@ -1,11 +1,32 @@
-import React from "react";
 import { Button, Text } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
+import { getAllBadges } from "../Api/getApi";
+import React, { useState, useEffect } from "react";
 
 export const BadgesScreen = ({ navigation }) => {
-    return (
-        <ScrollView>
-            <Text>Badges array</Text>
-        </ScrollView>
-    );
+	const [badges, setBadges] = useState([]);
+	useEffect(() => {
+		getAllBadges()
+			.then((badgesFromApi) => {
+				setBadges(badgesFromApi);
+			})
+			.then((err) => {
+				console.log(err);
+			});
+	}, []);
+	const list = () => {
+		return badges.map((badge) => {
+			return (
+				<ScrollView key={badge.name}>
+					<Text>{badge.name}</Text>
+				</ScrollView>
+			);
+		});
+	};
+	return <ScrollView>{list()}</ScrollView>;
 };
+// return (
+// 	<ScrollView>
+// 		<Text></Text>
+// 	</ScrollView>
+// );
