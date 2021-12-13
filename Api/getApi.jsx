d30bf9b1d80932d1d202cc27626ffb4301f684fd
JayAk12345 <jahanzaib50@hotmail.co.uk>
2021-12-13 16:52:24 +0000

    adds getUser to make get request for specific user

--- Api/getApi.jsx
+++ Api/getApi.jsx
@@ -4,8 +4,20 @@ const plantAppApi = axios.create({
   baseURL: "https://plant-app-b-end.herokuapp.com/api",
 });
 
-// export const getWeeklyPlants = () => {
-//   return plantAppApi.get("").then((res) => {
-//     return res.data.topics;
-//   });
-// };
+export const getUser = (userLogin) => {
+  return plantAppApi
+    .get(`users/${userLogin}`)
+    .then((res) => {
+      console.log(res.data.user);
+      return res.data.user;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export const getWeeklyPlants = () => {
+  return plantAppApi.get("").then((res) => {
+    return res.data.topics;
+  });
+};
