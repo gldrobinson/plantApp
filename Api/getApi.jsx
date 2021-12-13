3141e41c2857003ffb22a07aa2d2da02ddb6bef3
Holly <hollykgreaves@gmail.com>
2021-12-13 15:42:30 +0000

    adds getAllBadges function

--- Api/getApi.jsx
+++ Api/getApi.jsx
@@ -1,20 +1,26 @@
 import axios from "axios";
 
 const plantAppApi = axios.create({
-    baseURL: "https://plant-app-b-end.herokuapp.com/api",
+	baseURL: "https://plant-app-b-end.herokuapp.com/api",
 });
 
 export const addUser = () => {
-    return plantAppApi
-        .post("/users", {
-            username,
-            name,
-            badges,
-            currentWeek,
-            streak,
-            userplants,
-        })
-        .then((res) => {
-            console.log(res);
-        });
+	return plantAppApi
+		.post("/users", {
+			username,
+			name,
+			badges,
+			currentWeek,
+			streak,
+			userplants,
+		})
+		.then((res) => {
+			console.log(res);
+		});
+};
+
+export const getAllBadges = async () => {
+	return plantAppApi.get("/badges").then((res) => {
+		return res.data;
+	});
 };
