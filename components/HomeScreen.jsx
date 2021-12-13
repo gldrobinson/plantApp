26edf3a02880b39afc2201b43571d6bb4226baed
JayAk12345 <jahanzaib50@hotmail.co.uk>
2021-12-13 16:53:55 +0000

    removes weeklyCount component, HomeScreen shows current count and streak

--- components/HomeScreen.jsx
+++ components/HomeScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Text, View, StyleSheet, TextInput } from "react-native";
 import { userContext } from "../contexts/userContext";
 import { grains } from "../test-data/plants";
@@ -7,17 +7,22 @@ import { PlantsToGo } from "./PlantsToGo";
 import AutocompleteInput from "react-native-autocomplete-input";
 import { AutoInput } from "./AutoComplete";
 
-export const HomeScreen = () => {
-    const { user } = useContext(userContext);
-    const [weekCount, setWeekCount] = useState(0);
-    return (
-        <View style={styles.container}>
-            <Text>My Current Total:</Text>
-            <WeeklyCount weekCount={weekCount} setWeekCount={setWeekCount} />
-            <PlantsToGo weekCount={weekCount} />
-            <Text>Current streak 2 weeks!</Text>
-            <Text>Add new plant:</Text>
-            {/* <TextInput
+export const HomeScreen = ({
+  weekCount,
+  setWeekCount,
+  currentStreak,
+  setCurrentStreak,
+}) => {
+  const { user } = useContext(userContext);
+
+  return (
+    <View style={styles.container}>
+      <Text>My Current Total:</Text>
+      <Text style={styles.weeklyCount}>{weekCount}</Text>
+      <PlantsToGo weekCount={weekCount} />
+      <Text>Current streak {currentStreak} week(s)!</Text>
+      <Text>Add new plant:</Text>
+      {/* <TextInput
                 onChange={(e) => {
                     const input = e.nativeEvent.text;
                     setPlantToAdd(input);
@@ -30,22 +35,22 @@ export const HomeScreen = () => {
                     borderWidth: 1,
                 }}
             ></TextInput> */}
-            <AutoInput weekCount={weekCount} setWeekCount={setWeekCount} />
-        </View>
-    );
+      <AutoInput weekCount={weekCount} setWeekCount={setWeekCount} />
+    </View>
+  );
 };
 const styles = StyleSheet.create({
-    weeklyCount: {
-        fontSize: 100,
-        textAlign: "center",
-        justifyContent: "center",
-        alignItems: "center",
-        width: 200,
-        height: 200,
-        borderRadius: 200 / 2,
-        backgroundColor: "red",
-    },
-    container: {
-        alignItems: "center",
-    },
+  weeklyCount: {
+    fontSize: 100,
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    width: 200,
+    height: 200,
+    borderRadius: 200 / 2,
+    backgroundColor: "red",
+  },
+  container: {
+    alignItems: "center",
+  },
 });
