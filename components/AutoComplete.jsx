da98e58eaa4c71788b810dffe6fdc10e532f0119
Georgia <gldrobinson@hotmail.co.uk>
2021-12-13 21:57:19 +0000

    adds handleSubmit functionality

--- components/AutoComplete.jsx
+++ components/AutoComplete.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import {
   Button,
   StyleSheet,
@@ -9,14 +9,20 @@ import {
 } from "react-native";
 import AutocompleteInput from "react-native-autocomplete-input";
 import { getPlants } from "../Api/getApi";
+import { updateCurrentWeek } from "../Api/patchApi";
+import { userContext } from "../contexts/userContext";
 
 export const AutoInput = ({ weekCount, setWeekCount }) => {
+  const {user} = useContext(userContext)
   const [selectedValue, setSelectedValue] = useState("");
+  const [plantData, setPlantData] = useState([]);
   const [allPlants, setAllPlants] = useState([]);
+  const [placeholderText, setPlaceholderText] = useState("");
 
   useEffect(() => {
     getPlants()
       .then((plantsArr) => {
+        setPlantData(plantsArr)
         const plantNames = [];
         plantsArr.forEach((plant) => {
           plantNames.push(plant.name);
@@ -36,6 +42,29 @@ export const AutoInput = ({ weekCount, setWeekCount }) => {
       return str.includes(selectedValue);
     });
   }
+
+  const handleOnSubmit = () => {
+    if (selectedValue === "") {
+      setPlaceholderText("Don't forget to add your food before clicking submit!")
+    } else {
+      setPlaceholderText("")
+      let selectedPlant = plantData.filter((plant) => plant.name === selectedValue);
+      setWeekCount((currentCount) => {
+        console.log(currentCount, " in handleSubmit")
+        return currentCount + 1;
+      })
+      setSelectedValue("");
+      console.log(user, selectedPlant)
+      // update users week here... not currently working.
+      // updateCurrentWeek(user,selectedPlant).then((res) => {
+      //   console.log(res)
+      // if error, update placeholder text to "looks like you've already added that food for this week. Why not try something new!"
+      // })
+
+    }
+  }
+
+  console.log("weekCount = ",weekCount)
 
   return (
     <View style={styles.container}>
@@ -64,10 +93,9 @@ export const AutoInput = ({ weekCount, setWeekCount }) => {
           style={styles.button}
           color="#01937C"
           title="submit"
-          onPress={(weekCount) => {
-            setWeekCount(weekCount + 1);
-          }}
+          onPress={handleOnSubmit}
         ></Button>
+        <Text>{placeholderText}</Text>
       </View>
     </View>
   );
@@ -81,7 +109,7 @@ const styles = StyleSheet.create({
   },
   autocompleteContainer: {
     flex: 1,
-    backgroundColor: "white",
+    backgroundColor: "'FAF1E6'",
     width: 250,
   },
   button: {
