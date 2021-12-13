531a35e99e4e8a73b9e03b403122cb6dda4a79c2
JayAk12345 <jahanzaib50@hotmail.co.uk>
2021-12-13 11:53:29 +0000

    progresses suggested foods on week page

--- Api/getApi.jsx
+++ Api/getApi.jsx
@@ -1,3 +1,11 @@
-export const getWeeklyPlants = () => {
-    return ["1", "2", "3"];
-};
+import axios from "axios";
+
+const plantAppApi = axios.create({
+  baseURL: "https://plant-app-b-end.herokuapp.com/api",
+});
+
+// export const getWeeklyPlants = () => {
+//   return plantAppApi.get("").then((res) => {
+//     return res.data.topics;
+//   });
+// };
--- components/WeeklyCount.jsx
+++ components/WeeklyCount.jsx
@@ -1,37 +1,36 @@
 import React, { useEffect } from "react";
 import { getWeeklyPlants } from "../Api/getApi";
 import {
-    Button,
-    ScrollView,
-    Text,
-    View,
-    Image,
-    StyleSheet,
+  Button,
+  ScrollView,
+  Text,
+  View,
+  Image,
+  StyleSheet,
 } from "react-native";
 
 export const WeeklyCount = ({ weekCount, setWeekCount }) => {
-    // useEffect(() => {
-    // getWeeklyPlants().then((weeklyPlants) => {
-    // setWeekCount(weeklyPlants.length);
-    // });
-    // });
-    console.log(weekCount, "new");
-    setWeekCount(5);
+  //   useEffect(() => {
+  //     getWeeklyPlants().then((weeklyPlants) => {
+  //       setWeekCount(weeklyPlants.length);
+  //     });
+  //   });
 
-    return <Text style={styles.weeklyCount}>{weekCount}</Text>;
+  return <Text style={styles.weeklyCount}>{weekCount}</Text>;
 };
+
 const styles = StyleSheet.create({
-    weeklyCount: {
-        fontSize: 100,
-        textAlign: "center",
-        justifyContent: "center",
-        alignItems: "center",
-        width: 200,
-        height: 200,
-        borderRadius: 200 / 2,
-        backgroundColor: "red",
-    },
-    container: {
-        alignItems: "center",
-    },
+  weeklyCount: {
+    fontSize: 100,
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    width: 200,
+    height: 200,
+    borderRadius: 200 / 2,
+    backgroundColor: "red",
+  },
+  container: {
+    alignItems: "center",
+  },
 });
