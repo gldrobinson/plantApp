12c496812dde8cfcc3f74b61049d0a0fbbfbe29e
Georgia <gldrobinson@hotmail.co.uk>
2021-12-13 21:57:49 +0000

    adds styling

--- components/HomeScreen.jsx
+++ components/HomeScreen.jsx
@@ -5,18 +5,17 @@ import { PlantsToGo } from "./PlantsToGo";
 import { AutoInput } from "./AutoComplete";
 import { getUser } from "../Api/getApi";
 
-export const HomeScreen = ({
-  weekCount,
-  setWeekCount,
-  currentStreak,
-  setCurrentStreak,
-}) => {
+export const HomeScreen = () => {
   const { user } = useContext(userContext);
   const [userInfo, setUserInfo] = useState({})
+  const [weekCount, setWeekCount] = useState([])
+  const [currentStreak, setCurrentStreak] = useState(0)
 
   useEffect(() => {
     getUser(user).then((userData) => {
       setUserInfo(userData)
+      setWeekCount(userData.currentWeek.length)
+      setCurrentStreak(userData.streak.currentStreak)
     }).catch((err) => {
       console.log(err)
     })
@@ -26,40 +25,41 @@ export const HomeScreen = ({
 
   return (
     <View style={styles.container}>
-      <Text>My Current Total:</Text>
-      <Text style={styles.weeklyCount}>{weekCount}</Text>
-      <PlantsToGo weekCount={weekCount} />
+      <Text>My Week So Far:</Text>
+      <View style={styles.circleOverlay}>
+        <Text style={styles.weeklyCount}>{weekCount}</Text>
+      </View>
+      <Text style={styles.plantsToGo}>Only {30 - weekCount} to go!</Text>
       <Text>Current streak {currentStreak} week(s)!</Text>
       <Text>Add new plant:</Text>
-      {/* <TextInput
-                onChange={(e) => {
-                    const input = e.nativeEvent.text;
-                    setPlantToAdd(input);
-                    console.log(plantToAdd);
-                }}
-                style={{
-                    height: 30,
-                    width: 100,
-                    borderColor: "gray",
-                    borderWidth: 1,
-                }}
-            ></TextInput> */}
       <AutoInput weekCount={weekCount} setWeekCount={setWeekCount} />
     </View>
   );
 };
 const styles = StyleSheet.create({
-  weeklyCount: {
-    fontSize: 100,
-    textAlign: "center",
-    justifyContent: "center",
-    alignItems: "center",
-    width: 200,
-    height: 200,
-    borderRadius: 200 / 2,
-    backgroundColor: "red",
-  },
   container: {
     alignItems: "center",
+    flex : 1,
+    backgroundColor : "#FAF1E6",
+    justifyContent: "center",
+    alignItems: "center",
   },
+  circleOverlay : {
+    color: "#FFC074",
+    width: 100,
+    height: 100,
+    backgroundColor: "#01937C",
+    // borderRadius: 180 / 2,
+    justifyContent: "center",
+    borderWidth: 1,
+    padding: 30
+    
+  },
+  weeklyCount: {
+    fontSize: 80,
+    textAlign: "center",
+    fontWeight: "bold",
+    alignItems: "center",
+  },
+  
 });
