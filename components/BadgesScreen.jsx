ec76a758ed853746abc44486e3b45c75332d8418
Holly <hollykgreaves@gmail.com>
2021-12-13 15:55:58 +0000

    adds wfunction to display all badges

--- components/BadgesScreen.jsx
+++ components/BadgesScreen.jsx
@@ -1,4 +1,4 @@
-import { Button, Text } from "react-native";
+import { Button, Text, Image } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import { getAllBadges } from "../Api/getApi";
 import React, { useState, useEffect } from "react";
@@ -19,14 +19,13 @@ export const BadgesScreen = ({ navigation }) => {
 			return (
 				<ScrollView key={badge.name}>
 					<Text>{badge.name}</Text>
+					<Image
+						source={{ uri: badge.img_url }}
+						style={{ width: 100, height: 100 }}
+					/>
 				</ScrollView>
 			);
 		});
 	};
 	return <ScrollView>{list()}</ScrollView>;
 };
-// return (
-// 	<ScrollView>
-// 		<Text></Text>
-// 	</ScrollView>
-// );
