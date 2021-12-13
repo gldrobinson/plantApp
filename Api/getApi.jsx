d62b4d0f72bb490b25b62a2f85a607c8ddd9a844
J-Coke <1jamescoker1@gmail.com>
2021-12-13 10:51:22 +0000

    adds api file

--- Api/getApi.jsx
+++ Api/getApi.jsx
@@ -1,3 +1,20 @@
-export const getWeeklyPlants = () => {
-    return ["1", "2", "3"];
+import axios from "axios";
+
+const plantAppApi = axios.create({
+    baseURL: "https://plant-app-b-end.herokuapp.com/api",
+});
+
+export const addUser = () => {
+    return plantAppApi
+        .post("/users", {
+            username,
+            name,
+            badges,
+            currentWeek,
+            streak,
+            userplants,
+        })
+        .then((res) => {
+            console.log(res);
+        });
 };
