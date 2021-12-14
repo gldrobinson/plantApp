39ff5f9d98d62052089eb8ac25613a422b467045
Holly <hollykgreaves@gmail.com>
2021-12-14 17:08:29 +0000

    adds reset week

--- components/HomeScreen.jsx
+++ components/HomeScreen.jsx
@@ -5,65 +5,72 @@ import { PlantsToGo } from "./PlantsToGo";
 import { AutoInput } from "./AutoComplete";
 import { getUser } from "../Api/getApi";
 import badgeFunc from "../badge-utils";
+import { resetWeek } from "../Api/patchApi";
 
 export const HomeScreen = () => {
-  const { user } = useContext(userContext);
-  const [userInfo, setUserInfo] = useState({});
-  const [weekCount, setWeekCount] = useState([]);
-  const [currentStreak, setCurrentStreak] = useState(0);
+	const { user } = useContext(userContext);
+	const [userInfo, setUserInfo] = useState({});
+	const [weekCount, setWeekCount] = useState([]);
+	const [currentStreak, setCurrentStreak] = useState(0);
+	const date = new Date();
+	const day = date.getDay();
+	useEffect(() => {
+		getUser(user)
+			.then((userData) => {
+				setUserInfo(userData);
+				badgeFunc(userData);
+				setWeekCount(userData.currentWeek.length);
+				setCurrentStreak(userData.streak.currentStreak);
+			})
+			.then((userData) => {
+				if (day === 2) {
+					resetWeek(userData.username);
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+				console.log("error with get request on Home");
+			});
+	}, [user]);
 
-  useEffect(() => {
-    getUser(user)
-      .then((userData) => {
-        setUserInfo(userData);
-        badgeFunc(userData);
-        setWeekCount(userData.currentWeek.length);
-        setCurrentStreak(userData.streak.currentStreak);
-      })
-      .catch((err) => {
-        console.log(err);
-        console.log("error with get request on Home");
-      });
-  }, [user]);
-
-  return (
-    <View style={styles.container}>
-      <Text>My Week So Far:</Text>
-      <View style={styles.circleOverlay}>
-        <Text style={styles.weeklyCount}>{weekCount}</Text>
-      </View>
-      <Text style={styles.plantsToGo}>Only {30 - weekCount} to go!</Text>
-      <Text>Current streak {currentStreak} week(s)!</Text>
-      <Text>Add new plant:</Text>
-      <AutoInput weekCount={weekCount} setWeekCount={setWeekCount} />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text>My Week So Far:</Text>
+			<View style={styles.circleOverlay}>
+				<Text style={styles.weeklyCount}>{weekCount}</Text>
+			</View>
+			<Text style={styles.plantsToGo}>Only {30 - weekCount} to go!</Text>
+			<Text>Current streak {currentStreak} week(s)!</Text>
+			<Text>Add new plant:</Text>
+			<AutoInput weekCount={weekCount} setWeekCount={setWeekCount} />
+		</View>
+	);
 };
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#FAF1E6",
-  },
+	container: {
+		flex: 1,
+		flexDirection: "column",
+		alignItems: "center",
+		justifyContent: "center",
+		backgroundColor: "#FAF1E6",
+	},
 
-  circleOverlay: {
-    color: "#FFC074",
-    width: 180,
-    height: 180,
-    backgroundColor: "#01937C",
-    borderRadius: 180 / 2,
-    justifyContent: "center",
-    alignItems: "center",
-    borderWidth: 1,
-  },
+	circleOverlay: {
+		color: "#FFC074",
+		width: 180,
+		height: 180,
+		backgroundColor: "#01937C",
+		borderRadius: 180 / 2,
+		justifyContent: "center",
+		alignItems: "center",
+		borderWidth: 1,
+	},
 
-  weeklyCount: {
-    fontSize: 80,
-    // textAlign: "center",
-    fontWeight: "bold",
-    alignItems: "center",
-    justifyContent: "centre",
-  },
+	weeklyCount: {
+		fontSize: 80,
+		// textAlign: "center",
+		fontWeight: "bold",
+		alignItems: "center",
+		justifyContent: "centre",
+	},
 });
