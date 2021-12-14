4b96ea552679f01ff6218c3db5b23d84f5bf63dd
J-Coke <1jamescoker1@gmail.com>
2021-12-14 14:50:10 +0000

    adds some styling to week screen

--- components/FoodCards.jsx
+++ components/FoodCards.jsx
@@ -1,23 +1,41 @@
 import React from "react";
-import { Text, View, Image, ScrollView } from "react-native";
+import { Text, View, Image, ScrollView, StyleSheet } from "react-native";
 
 export const FoodCards = ({ displayPlants }) => {
     return (
-        <ScrollView horizontal={true}>
+        <ScrollView horizontal={true} style={styles.container}>
             {displayPlants.map((plant) => {
                 return (
                     <View key={plant._id}>
-                        <Image
-                            style={{ width: 200, height: 200 }}
-                            source={{
-                                uri: plant.img_url,
+                        <View style={styles.imageBackground}>
+                            <Image
+                                style={styles.foodPic}
+                                source={{
+                                    uri: plant.img_url,
+                                }}
+                            />
+                        </View>
+                        <Text
+                            style={{
+                                textAlign: "center",
+                                fontFamily: "System",
+                                fontSize: 18,
+                                fontWeight: "bold",
+                                textTransform: "capitalize",
                             }}
-                        />
-                        <Text style={{ textAlign: "center" }}>
+                        >
                             {plant.name}
                         </Text>
-                        <Text style={{ textAlign: "center" }}>
-                            {plant.category}
+                        <Text
+                            style={{
+                                textAlign: "center",
+                                fontFamily: "System",
+                                fontSize: 14,
+                                fontStyle: "italic",
+                                textTransform: "capitalize",
+                            }}
+                        >
+                            Category: {plant.category}
                         </Text>
                     </View>
                 );
@@ -25,3 +43,30 @@ export const FoodCards = ({ displayPlants }) => {
         </ScrollView>
     );
 };
+
+//capitalise
+const styles = StyleSheet.create({
+    foodPic: {
+        borderWidth: 4,
+        borderColor: "#01937C",
+        borderRadius: 100,
+        width: "90%",
+        height: "90%",
+        marginTop: "auto",
+        marginBottom: "auto",
+    },
+    imageBackground: {
+        // borderWidth: 4,
+        // borderColor: "#20232a",
+        // borderRadius: 6,
+        backgroundColor: "#FAF1E6",
+        width: 200,
+        height: 200,
+        alignItems: "center",
+        alignContent: "center",
+    },
+    container: {
+        alignContent: "center",
+        backgroundColor: "#FAF1E6",
+    },
+});
