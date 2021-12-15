efe0560a24060bace489420861504ea768422ca4
JayAk12345 <jahanzaib50@hotmail.co.uk>
2021-12-15 18:21:15 +0000

    styles the signin/signup overlay

--- components/SignInOverlay.jsx
+++ components/SignInOverlay.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useContext } from "react";
-import { View, Text, TextInput, Button } from "react-native";
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
 import { Overlay } from "react-native-elements";
 import { useForm } from "react-hook-form";
 import { userContext } from "../contexts/userContext";
@@ -10,111 +10,150 @@ import { Image } from "react-native-elements";
 // import { updateUser } from "../contexts/userContext";
 
 export const SignInOverlay = ({
-	weekCount,
-	setWeekCount,
-	setCurrentStreak,
-	signUpDate,
-	setSignUpDate,
-	surrentDate,
-	setCurrentDate,
+  weekCount,
+  setWeekCount,
+  setCurrentStreak,
+  signUpDate,
+  setSignUpDate,
+  surrentDate,
+  setCurrentDate,
 }) => {
-	const [visible, setVisible] = useState(true);
-	const [userLogin, setUserLogin] = useState("");
-	const { user, updateUser } = useContext(userContext);
-	const [newUsername, setNewUsername] = useState("");
-	const [newName, setNewName] = useState("");
-	const [usernameMessage, setUsernameMessage] = useState("");
-	const [signUpMessage, setSignUpMessage] = useState("");
-	const logoUrl = "https://i.postimg.cc/rpB4dCn6/logo-rooting.png";
+  const [visible, setVisible] = useState(true);
+  const [userLogin, setUserLogin] = useState("");
+  const { user, updateUser } = useContext(userContext);
+  const [newUsername, setNewUsername] = useState("");
+  const [newName, setNewName] = useState("");
+  const [usernameMessage, setUsernameMessage] = useState("");
+  const [signUpMessage, setSignUpMessage] = useState("");
+  const logoUrl = "https://i.postimg.cc/rpB4dCn6/logo-rooting.png";
 
-	const toggleOverlay = () => {
-		if (!user) {
-			setVisible(true);
-		} else {
-			setVisible(false);
-		}
-	};
+  const toggleOverlay = () => {
+    if (!user) {
+      setVisible(true);
+    } else {
+      setVisible(false);
+    }
+  };
 
-	useEffect(() => {
-		toggleOverlay();
-	}, [user]);
+  useEffect(() => {
+    toggleOverlay();
+  }, [user]);
 
-	const handleSignIn = () => {
-		if (userLogin === "") {
-			setUsernameMessage("Please add your username");
-		} else {
-			getUser(userLogin)
-				.then((user) => {
-					updateUser(user.username);
-					setWeekCount(user.currentWeek.length);
-					setCurrentStreak(user.streak.currentStreak);
-					setCurrentDate(Date.now());
-				})
-				.catch((err) => {
-					setVisible(true);
-					setUsernameMessage("Username not found");
-				});
-		}
-	};
+  const handleSignIn = () => {
+    if (userLogin === "") {
+      setUsernameMessage("Please add your username");
+    } else {
+      getUser(userLogin)
+        .then((user) => {
+          updateUser(user.username);
+          setWeekCount(user.currentWeek.length);
+          setCurrentStreak(user.streak.currentStreak);
+          setCurrentDate(Date.now());
+        })
+        .catch((err) => {
+          setVisible(true);
+          setUsernameMessage("Username not found");
+        });
+    }
+  };
 
-	const handleSignUp = () => {
-		if (newName === "" || newUsername === "") {
-			setSignUpMessage("Please enter your name and username");
-		} else {
-			addUser({
-				username: newUsername,
-				name: newName,
-			}).then((user) => {
-				updateUser(user.username);
-				setWeekCount(user.currentWeek.length);
-				setCurrentStreak(user.streak.currentStreak);
-				setSignUpDate(getDateForTimer());
-			});
-		}
-	};
+  const handleSignUp = () => {
+    if (newName === "" || newUsername === "") {
+      setSignUpMessage("Please enter your name and username");
+    } else {
+      addUser({
+        username: newUsername,
+        name: newName,
+      }).then((user) => {
+        updateUser(user.username);
+        setWeekCount(user.currentWeek.length);
+        setCurrentStreak(user.streak.currentStreak);
+        setSignUpDate(getDateForTimer());
+      });
+    }
+  };
 
-	return (
-		<View>
-			<Overlay isVisible={visible}>
-				<Image
-					source={{
-						uri: logoUrl,
-					}}
-					style={{ width: 300, height: 100, resizeMode: "contain" }}
-					placeholderStyle={{ backgroundColor: "transparent" }}
-				/>
-				<Text>Sign in:</Text>
-				<TextInput
-					placeholder="username"
-					onChange={(e) => {
-						const input = e.nativeEvent.text;
-						setUserLogin(input);
-					}}
-					style={{ height: 30, borderColor: "gray", borderWidth: 1 }}
-				></TextInput>
-				<Text>{usernameMessage}</Text>
-				<Button title="Submit" onPress={handleSignIn}></Button>
+  return (
+    <View style={{ padding: 10 }}>
+      <Overlay isVisible={visible}>
+        <Image
+          source={{
+            uri: logoUrl,
+          }}
+          style={{ width: 300, height: 100, resizeMode: "contain" }}
+          placeholderStyle={{ backgroundColor: "transparent" }}
+        />
+        <Text style={{ padding: 8 }}>Sign in:</Text>
+        <TextInput
+          placeholder="username"
+          onChange={(e) => {
+            const input = e.nativeEvent.text;
+            setUserLogin(input);
+          }}
+          style={{ height: 30, borderColor: "gray", borderWidth: 1 }}
+        ></TextInput>
+        <Text>{usernameMessage}</Text>
+        <View
+          style={{
+            width: "100px",
+            marginLeft: "32.5%",
+            marginRight: "32.5%",
+          }}
+        >
+          <Button
+            title="Sign In"
+            color="#01937C"
+            onPress={handleSignIn}
+          ></Button>
+        </View>
 
-				<Text>Sign up:</Text>
-				<TextInput
-					placeholder="Username"
-					onChange={(e) => {
-						const input = e.nativeEvent.text;
-						setNewUsername(input);
-					}}
-					style={{ height: 30, borderColor: "gray", borderWidth: 1 }}
-				></TextInput>
-				<TextInput
-					placeholder="First name"
-					onChange={(e) => {
-						const input = e.nativeEvent.text;
-						setNewName(input);
-					}}
-					style={{ height: 30, borderColor: "gray", borderWidth: 1 }}
-				></TextInput>
-				<Text>{signUpMessage}</Text>
-				<Button title="Submit" onPress={handleSignUp}></Button>
-			</Overlay>
-		</View>
-	);
+        <Text style={{ padding: 8 }}>Sign up:</Text>
+        <TextInput
+          placeholder="Username"
+          onChange={(e) => {
+            const input = e.nativeEvent.text;
+            setNewUsername(input);
+          }}
+          style={{ height: 30, borderColor: "gray", borderWidth: 1 }}
+        ></TextInput>
+        <TextInput
+          placeholder="First name"
+          onChange={(e) => {
+            const input = e.nativeEvent.text;
+            setNewName(input);
+          }}
+          style={{ height: 30, borderColor: "gray", borderWidth: 1 }}
+        ></TextInput>
+        <Text>{signUpMessage}</Text>
+        <View
+          style={{
+            width: "100px",
+            marginLeft: "32.5%",
+            marginRight: "32.5%",
+          }}
+        >
+          <Button
+            title="Sign Up"
+            color="#01937C"
+            onPress={handleSignUp}
+          ></Button>
+        </View>
+      </Overlay>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    position: "relative",
+    flex: 1,
+    paddingTop: 1000,
+    justifyContent: "space-between",
+    marginBottom: 1000,
+    // flexDirection: "column",
+    // padding: 10,
+  },
+  signIn: {
+    borderWidth: 2,
+  },
+});
