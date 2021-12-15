4c439e05d1695af44b458a19b7e4e46bc53c7e52
Holly <hollykgreaves@gmail.com>
2021-12-15 12:03:50 +0000

    amends issue with badges showing

--- components/HomeScreen.jsx
+++ components/HomeScreen.jsx
@@ -9,143 +9,153 @@ import badgeFunc from "../badge-utils";
 import { resetWeek } from "../Api/patchApi";
 
 export const HomeScreen = () => {
-    const { user } = useContext(userContext);
-    const [userInfo, setUserInfo] = useState({});
-    const [weekCount, setWeekCount] = useState([]);
-    const [currentStreak, setCurrentStreak] = useState(0);
-    const [highestStreak, setHighestStreak] = useState(0);
-    const [newBadge, setNewBadge] = useState("");
-    const date = new Date();
-    const day = date.getDay();
+	const { user } = useContext(userContext);
+	const [userInfo, setUserInfo] = useState({});
+	const [weekCount, setWeekCount] = useState([]);
+	const [currentStreak, setCurrentStreak] = useState(0);
+	const [highestStreak, setHighestStreak] = useState(0);
+	const [displayedBadge, setDisplayedBadge] = useState(false);
+	const [newBadge, setNewBadge] = useState("");
+	const date = new Date();
+	const day = date.getDay();
 
-    useEffect(() => {
-        getUser(user)
-            .then((userData) => {
-                setUserInfo(userData);
-                setNewBadge(badgeFunc(userData));
-                setWeekCount(userData.currentWeek.length);
-                setCurrentStreak(userData.streak.currentStreak);
-            })
-            .then((userData) => {
-                if (day === 0) {
-                    resetWeek(userData.username);
-                }
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    }, [user]);
-    useEffect(() => {
-        if (weekCount === 30) {
-            console.log("in week count use effect");
-            setCurrentStreak(currentStreak + 1);
-            updateStreak(user, true)
-                .catch((err) => {
-                    setCurrentStreak(currentStreak - 1);
-                    console.dir(err);
-                });
-        }
-    }, [weekCount]);
+	useEffect(() => {
+		getUser(user)
+			.then((userData) => {
+				setUserInfo(userData);
+				setNewBadge(badgeFunc(userData));
+				setWeekCount(userData.currentWeek.length);
+				setCurrentStreak(userData.streak.currentStreak);
+			})
+			.then((userData) => {
+				if (day === 0) {
+					resetWeek(userData.username);
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, [user]);
+	useEffect(() => {
+		if (weekCount === 30) {
+			setCurrentStreak(currentStreak + 1);
+			updateStreak(user, true).catch((err) => {
+				setCurrentStreak(currentStreak - 1);
+				console.dir(err);
+			});
+		}
+	}, [weekCount]);
 
+	const badge = () => {
+		if (newBadge) {
+			setDisplayedBadge === true;
+			return <Text style={styles.badges}>{newBadge}</Text>;
+		} else "";
+	};
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.weekTitle}>My Week So Far</Text>
-      <View style={styles.circleOverlay}>
-        <Text style={styles.weeklyCount}>{weekCount}</Text>
-      </View>
-      <View style={styles.streakContainer}>
-        <View style={styles.currentStreak}>Streak : {currentStreak}</View>
-        <View style={styles.space}></View>
-        <View style={styles.highestStreak}>Highest Streak: {highestStreak}</View>
-      </View>
-      <Text style={styles.plantsToGo}> {30 - weekCount > 0 ? `Only ${30 - weekCount} to go!`: `Congratulations! You made your 30 for the week!`}</Text>
-      <Text style={styles.badges}>{newBadge ? newBadge : ""}</Text>
-      <Text style={styles.addPlant}>Add a new plant</Text>
-      <AutoInput weekCount={weekCount} setWeekCount={setWeekCount} />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text style={styles.weekTitle}>My Week So Far</Text>
+			<View style={styles.circleOverlay}>
+				<Text style={styles.weeklyCount}>{weekCount}</Text>
+			</View>
+			<View style={styles.streakContainer}>
+				<View style={styles.currentStreak}>Streak : {currentStreak}</View>
+				<View style={styles.space}></View>
+				<View style={styles.highestStreak}>
+					Highest Streak: {highestStreak}
+				</View>
+			</View>
+			<Text style={styles.plantsToGo}>
+				{" "}
+				{30 - weekCount > 0
+					? `Only ${30 - weekCount} to go!`
+					: `Congratulations! You made your 30 for the week!`}
+			</Text>
+			{badge()}
+			<Text style={styles.addPlant}>Add a new plant</Text>
+			<AutoInput weekCount={weekCount} setWeekCount={setWeekCount} />
+		</View>
+	);
 };
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#FAF1E6",
-    },
-    weekTitle: {
-        fontFamily: "System",
-        fontSize: 26,
-        fontWeight: "bold",
-        padding: 15,
-    },
-    circleOverlay: {
-        color: "#FFC074",
-        width: 180,
-        height: 180,
-        backgroundColor: "#01937C",
-        borderRadius: 180 / 2,
-        justifyContent: "center",
-        alignItems: "center",
-        borderWidth: 2,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 11,
-        },
-        shadowOpacity: 0.55,
-        shadowRadius: 14.78,
-    },
-    weeklyCount: {
-        fontSize: 80,
-        fontWeight: "bold",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    streakContainer: {
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-evenly",
-        height: 70,
-        paddingTop: 20
-    },
-    currentStreak: {
-        width: 150,
-        height: 50,
-        backgroundColor: "#FFC074",
-        borderRadius: 5,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    space : {
-      padding: 10,
-    },
-    highestStreak: {
-        width: 150,
-        height: 50,
-        backgroundColor: "#FFC074",
-        borderRadius: 5,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    plantsToGo: {
-        paddingTop: 20,
-        fontSize: 20,
-        paddingBottom: 10,
-    },
-    badges : {
-
-      borderWidth: 1,
-      fontSize: 14,
-      textAlign : "center",
-      padding: 10
-    },
-    addPlant: {
-        fontFamily: "System",
-        fontSize: 20,
-        fontWeight: "bold",
-        paddingTop: 15,
-    },
-
+	container: {
+		flex: 1,
+		flexDirection: "column",
+		alignItems: "center",
+		justifyContent: "center",
+		backgroundColor: "#FAF1E6",
+	},
+	weekTitle: {
+		fontFamily: "System",
+		fontSize: 26,
+		fontWeight: "bold",
+		padding: 15,
+	},
+	circleOverlay: {
+		color: "#FFC074",
+		width: 180,
+		height: 180,
+		backgroundColor: "#01937C",
+		borderRadius: 180 / 2,
+		justifyContent: "center",
+		alignItems: "center",
+		borderWidth: 2,
+		shadowColor: "#000",
+		shadowOffset: {
+			width: 0,
+			height: 11,
+		},
+		shadowOpacity: 0.55,
+		shadowRadius: 14.78,
+	},
+	weeklyCount: {
+		fontSize: 80,
+		fontWeight: "bold",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	streakContainer: {
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "space-evenly",
+		height: 70,
+		paddingTop: 20,
+	},
+	currentStreak: {
+		width: 150,
+		height: 50,
+		backgroundColor: "#FFC074",
+		borderRadius: 5,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	space: {
+		padding: 10,
+	},
+	highestStreak: {
+		width: 150,
+		height: 50,
+		backgroundColor: "#FFC074",
+		borderRadius: 5,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	plantsToGo: {
+		paddingTop: 20,
+		fontSize: 20,
+		paddingBottom: 10,
+	},
+	badges: {
+		borderWidth: 1,
+		fontSize: 14,
+		textAlign: "center",
+		padding: 10,
+	},
+	addPlant: {
+		fontFamily: "System",
+		fontSize: 20,
+		fontWeight: "bold",
+		paddingTop: 15,
+	},
 });
