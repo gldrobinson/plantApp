abed507fc1c8fc8313b00a0a6973dc8bde42fb91
Holly <hollykgreaves@gmail.com>
2021-12-16 09:25:16 +0000

    amends logout issue

--- components/Logout.jsx
+++ components/Logout.jsx
@@ -3,25 +3,25 @@ import { Button, Text, View, StyleSheet } from "react-native";
 import { userContext } from "../contexts/userContext";
 
 export const Logout = () => {
-    const { user, updateUser } = useContext(userContext);
-    const handlePress = () => {
-        updateUser(null);
-    };
-    return (
-        <View style={styles.container}>
-            <Button title="LOGOUT" onPress={handlePress} color="black"></Button>
-        </View>
-    );
+	const { user, updateUser } = useContext(userContext);
+	const handlePress = () => {
+		updateUser("");
+	};
+	return (
+		<View style={styles.container}>
+			<Button title="LOGOUT" onPress={handlePress} color="black"></Button>
+		</View>
+	);
 };
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: "#FAF1E6",
-        paddingRight: 20,
-    },
-    // logout: {
-    //     width: 10,
-    //     color: "black",
-    //     alignItems: "flex-end",
+	container: {
+		backgroundColor: "#FAF1E6",
+		paddingRight: 20,
+	},
+	// logout: {
+	//     width: 10,
+	//     color: "black",
+	//     alignItems: "flex-end",
 
-    // },
+	// },
 });
